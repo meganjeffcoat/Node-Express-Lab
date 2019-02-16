c7c4b48c8c5e20b6cf97334ce772dc10842731d4
Megan Jeffcoat <jeffcoat.megan@gmail.com>
2019-02-16 17:07:01 -0600

    server.js updated for router

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
+const postsRouter = require('./router/PostRouter');
+
 const server = express();
 
 server.use(express.json());
+server.use('/api/posts', postsRouter);
 
 server.get('/', async (req, res) => {
     res.send(`<h1>Testing</h1>`)
